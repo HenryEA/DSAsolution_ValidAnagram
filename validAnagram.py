3eaac184c6eb03a9062ca2bcc9fd677bc543546c
Henry <anene.hekene@gmail.com>
2024-08-04 12:52:59 +0100

    initial commit

---
+++ validAnagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+          return False
+        sortedS = "".join(sorted(s))
+        sortedT = "".join(sorted(t))
+        
+        for i in range(len(sortedS)):
+            if sortedS[i] != sortedT[i]:
+                return False
+            
+        return True
